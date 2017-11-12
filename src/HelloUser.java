221eecb478d3a3dc3296add202603291e8a615ca
Cristian Dragnea <cristian.dragnea@tum.de>
2017-11-12 23:06:26 +0100

    Finishing Exercise 1

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -1,4 +1,15 @@
 
 public class HelloUser {
-
+	
+	private String userName;
+	
+	public HelloUser(String s_userName)
+	{
+		userName = s_userName;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello "+userName+" !");
+	}
+	
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,12 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello ASE2017 - How are you now? :)");
+		
+		HelloUser h = new HelloUser("Cristian");
+		
+		h.greetUser();
 	}
 
 }
