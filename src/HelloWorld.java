5d8f3804c6aa290f28a0a40c4b7953572a919d79
Cristian Dragnea <cristian.dragnea@tum.de>
2017-11-12 23:37:38 +0100

    Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello ASE2017 - How are you now? :)");
 		
-		HelloUser h = new HelloUser("Cristian");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Hello ASE2017 - How are you now? :)");
+		System.out.println("\nPlease enter your name: ");
+		String name = sc.next();
+				
+		HelloUser h = new HelloUser(name);
 		
 		h.greetUser();
+		
+		sc.close();
 	}
 
 }
